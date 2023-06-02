07e84a232a0b522c152f395460b85a96c3bba65c
J4rdanAG <Jordanarchiegrahn@gmail.com>
2023-06-02 04:16:38 -0400

    added custom routes, modified existing Druid Profile, pre-merge commit

--- BlazorServer/run.bat
+++ BlazorServer/run.bat
@@ -1,5 +1,5 @@
-start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://localhost:5000"
-cd /D "%~dp0"
+start "C:\Program Files (x64)\Google\Chrome\Application\chrome.exe" "http://localhost:5000"
+c:
+cd C:\WowClassicGrindBot\WowClassicGrindBot\BlazorServer
 dotnet run --configuration Release
-
 pause
